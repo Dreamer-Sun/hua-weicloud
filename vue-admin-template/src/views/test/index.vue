530ae03e1bbc4809dad28d63020260b3a1b61bdf
Eternal <liujiaqang@outlook.com>
2022-06-07 18:28:52 +0800

    1

---
+++ vue-admin-template/src/views/test/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+      <el-dialog
+        title="提示"
+        :visible.sync="dialogVisible"
+        width="30%"
+        :before-close="handleClose">
+        <span>{{this.tokenin}}</span>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        </span>
+      </el-dialog>
+      <el-button
+      type="primary"
+      style="margin-left: 20px"
+      @click="Showtoken(); dialogVisible = true"
+      >显示Token</el-button>
+</div>
+</template>
+
+<script>
+import {
+  show_token
+} from "@/api/tok";
+
+export default {
+  data(){
+    return{
+      name: "index",
+      tokenin: '',
+      dialogVisible: false,
+    }
+  },
+  created() {
+    console.log("here is nothing")
+  },
+  methods: {
+    Showtoken(){
+      show_token().then((res) => {
+        console.log("This is token")
+        this.tokenin = res.token;
+        console.log(this.tokenin);
+      })
+    },
+    handleClose(done) {
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+      }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
