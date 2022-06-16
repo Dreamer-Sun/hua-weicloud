6fa0869638ff4b29ce0401b36b03da2548f7fb53
sunjw <1792107188@qq.com>
2022-06-16 21:28:47 +0800

    站点显示bug

--- hwcloud/tnmp/manage/querysites.py
+++ hwcloud/tnmp/manage/querysites.py
@@ -15,112 +15,136 @@ tokenid = Get_Token()
 headers = {
     "X-ACCESS-TOKEN": tokenid
 }
-
+params = {
+        "pageIndex": 1,
+        "pageSize": 100
+          }
 def GetSiteId():
-    res = requests.get(url='https://cn2.naas.huaweicloud.com:18002/controller/campus/v3/sites',
-                       headers=headers)
-    res_data = res.json()
-    data = res_data.get('data')
     SiteIdList = []
-    for i in range(len(data)):
-        SiteIdList.append(data[i]["id"])
+    res = requests.get(url='https://cn2.naas.huaweicloud.com:18002/controller/campus/v3/sites',
+                       headers=headers,)
+    total = res.json().get('totalRecords')
+    num = int((int(total) / 100)) + 1
+    for i in range(num):
+        params["pageIndex"] = i + 1
+        res = requests.get(url='https://cn2.naas.huaweicloud.com:18002/controller/campus/v3/sites',
+                           headers=headers,
+                           params=params)
+        res_data = res.json()
+        data = res_data.get('data')
+        for j in range(len(data)):
+            SiteIdList.append(data[j]["id"])
 
-    # print(SiteIdList)
+
+    print(len(SiteIdList))
+    print(SiteIdList)
     return SiteIdList
 
 
 def QuerySites():
+    SiteDataList = []
     res = requests.get(url='https://cn2.naas.huaweicloud.com:18002/controller/campus/v3/sites',
-                       headers=headers)
-    res_data = res.json()
-    data = res_data.get('data')
-    # print(data[0]["id"])
-    print(len(data))
+                       headers=headers,)
+    total = res.json().get('totalRecords')
+    num = int((int(total) / 100)) + 1
+    for i in range(num):
+        params["pageIndex"] = i + 1
+        res = requests.get(url='https://cn2.naas.huaweicloud.com:18002/controller/campus/v3/sites',
+                           headers=headers,
+                           params=params)
+        res_data = res.json()
+        data = res_data.get('data')
+        # print("data", data)
+        for j in range(len(data)):
+            SiteDataList.append(data[j])
+
+    print("len(SiteDataList)", len(SiteDataList))
+
     after_data = []
     child = {}
     num = 0
     children = {}
     children2 = []
-    for i in range(len(data)):
+    for i in range(len(SiteDataList)):
         children2.append([])
 
-    for i in range(len(data)):
+    for i in range(len(SiteDataList)):
         children["id"] = num
         num = num + 1
-        children["label"] = "tenantId：" + str(data[i]["tenantId"])
+        children["label"] = "tenantId：" + str(SiteDataList[i]["tenantId"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "name：" + str(data[i]["name"])
+        children["label"] = "name：" + str(SiteDataList[i]["name"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "description：" + str(data[i]["description"])
+        children["label"] = "description：" + str(SiteDataList[i]["description"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "type：" + str(data[i]["type"])
+        children["label"] = "type：" + str(SiteDataList[i]["type"])
         children2[i].append(children)
         children["id"] = num
         children = {}
         num = num + 1
-        children["label"] = "latitude：" + str(data[i]["latitude"])
+        children["label"] = "latitude：" + str(SiteDataList[i]["latitude"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "longtitude：" + str(data[i]["longtitude"])
+        children["label"] = "longtitude：" + str(SiteDataList[i]["longtitude"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "longitude：" + str(data[i]["longitude"])
+        children["label"] = "longitude：" + str(SiteDataList[i]["longitude"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "contact：" + str(data[i]["contact"])
+        children["label"] = "contact：" + str(SiteDataList[i]["contact"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "tag：" + str(data[i]["tag"])
+        children["label"] = "tag：" + str(SiteDataList[i]["tag"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "isolated：" + str(data[i]["isolated"])
+        children["label"] = "isolated：" + str(SiteDataList[i]["isolated"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "email：" + str(data[i]["email"])
+        children["label"] = "email：" + str(SiteDataList[i]["email"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "phone：" + str(data[i]["phone"])
+        children["label"] = "phone：" + str(SiteDataList[i]["phone"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "postcode：" + str(data[i]["postcode"])
+        children["label"] = "postcode：" + str(SiteDataList[i]["postcode"])
         children2[i].append(children)
         children = {}
         children["id"] = num
         num = num + 1
-        children["label"] = "address：" + str(data[i]["address"])
+        children["label"] = "address：" + str(SiteDataList[i]["address"])
         children2[i].append(children)
         children = {}
     print(children2[0])
 
-    for i in range(len(data)):
+    for i in range(len(SiteDataList)):
         child["id"] = num
         num = num + 1
-        child["label"] = "设备id：" + data[i]["id"]
+        child["label"] = "设备id：" + SiteDataList[i]["id"]
         child["children"] = children2[i]
         after_data.append(child)
         child = {}
